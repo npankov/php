d8a9aab80ce49774793113e14810376877837807
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-27 10:37:16 +0100

    exercise 7, 8, 9

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 <body>
     <table>
          <?php
-         include 'variables.php';
-         include 'functions.php';
+         include './includes/variables.php';
+         include './includes/functions.php';
 
             foreach($myArray as $element) {
                 renderLine($element);
@@ -21,10 +21,19 @@
 </html>
 
 
-<!-- 6. Séparation des fichiers
-    Nous allons maintenant séparer notre code en plusieurs éléments, à thème. L'idée est ici d'avoir des fichiers plus spécialisés et rapides à retrouver.
-    Séparer le code en plusieurs fichiers php :
-        Un pour les variables
-        Un autre pour les fonctions
-        Un pour le HTML
-    Appeler ces fichiers dans le HTML (utiliser include pour appeler les différents fichiers (opens new window) où vous le souhaitez) -->
+<!-- 7. Utiliser git
+8. Mise en place d'un layout
+
+Ce que j'appelle layout : nous allons découper notre HTML pour en extraire les éléments communs (balises <html>, <head>, <body>, etc.) et les mettre dans des fichiers qui vont être inclus dans toutes nos pages.
+
+    Créer un dossier includes et y ajouter les fichiers contenant les variables et les fonctions
+    Dans ce dossier, y ajouter un fichier header.php et un fichier footer.php
+
+#
+9. Header
+
+L'idée est ici de regrouper la plus grande partie du code commun, pour l'inclure dans toutes nos pages (et donc, réduire le nombre de fois où on a besoin de l'écrire).
+
+    Dans header.php, déplacer les includes des fichiers de variables et de fonctions (remplacer les includes par des require_once et rechercher ce que fait cette fonction)
+    Ajouter le html qui va être commun à toutes nos pages (balises <html>, <head>, <body>, etc.)
+    Inclure le fichier header.php dans la page de liste des bonnets -->
