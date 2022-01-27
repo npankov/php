b97d5d51bd30d7df3f5a5653b9d1ab5bb4807a3a
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-27 11:09:39 +0100

    exercise 9, 10

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+require_once './includes/header.php'
+?>
     <table>
          <?php
          include './includes/variables.php';
@@ -17,23 +11,19 @@
             }
         ?>
     </table>
-</body>
-</html>
+<?php
+require_once './includes/footer.php'
+?>
 
 
-<!-- 7. Utiliser git
-8. Mise en place d'un layout
+<!-- 9. Header
 
-Ce que j'appelle layout : nous allons découper notre HTML pour en extraire les éléments communs (balises <html>, <head>, <body>, etc.) et les mettre dans des fichiers qui vont être inclus dans toutes nos pages.
-
-    Créer un dossier includes et y ajouter les fichiers contenant les variables et les fonctions
-    Dans ce dossier, y ajouter un fichier header.php et un fichier footer.php
-
-#
-9. Header
-
-L'idée est ici de regrouper la plus grande partie du code commun, pour l'inclure dans toutes nos pages (et donc, réduire le nombre de fois où on a besoin de l'écrire).
-
+    L'idée est ici de regrouper la plus grande partie du code commun, pour l'inclure dans toutes nos pages (et donc, réduire le nombre de fois où on a besoin de l'écrire).
     Dans header.php, déplacer les includes des fichiers de variables et de fonctions (remplacer les includes par des require_once et rechercher ce que fait cette fonction)
     Ajouter le html qui va être commun à toutes nos pages (balises <html>, <head>, <body>, etc.)
-    Inclure le fichier header.php dans la page de liste des bonnets -->
+    Inclure le fichier header.php dans la page de liste des bonnets
+
+    10. Footer
+    Dans footer.php, ajouter le html commun à toutes nos pages se trouvant après le php
+    Inclure ce fichier footer.php dans la page de liste
+    -->
