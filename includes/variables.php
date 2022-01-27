53d77284b5bbc4df9f36673dab490de9d8a98e0c
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-27 13:58:43 +0100

    exercise 11, 12, 13

--- includes/variables.php
+++ includes/variables.php
@@ -1,8 +1,24 @@
 <?php
 $myArray = [
-    ['Bonnet en laine', 10, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.'],
-    ['Bonnet en laine bio', 14, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.'],
-    ['Bonnet en laine et cachemire', 20, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.'],
-    ['Bonnet arc-en-ciel', 12, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.']
+    ['Bonnet en laine',
+        10,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.',
+        './includes/img/1.jpg'
+        ],
+    ['Bonnet en laine bio',
+        14,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.',
+        './includes/img/2.jpg'
+        ],
+    ['Bonnet en laine et cachemire',
+        20,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.',
+        './includes/img/3.jpg'
+        ],
+    ['Bonnet arc-en-ciel',
+        12,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida.',
+        './includes/img/4.jpg'
+        ]
 ];
 
--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
-require_once './includes/header.php'
+    include_once './includes/header.php';
 ?>
-    <table>
-         <?php
-         include './includes/variables.php';
-         include './includes/functions.php';
 
-            foreach($myArray as $element) {
-                renderLine($element);
+<div class="row">
+    <?php
+        $count = 0;
+        foreach ($myArray as $element) {
+            $count++;
+            ?>
+
+            <div class="card mx-2" style="width: 18rem;">
+              <img src="<?= $element[3]; ?>" class="card-img-top" alt="...">
+              <div class="card-body d-flex justify-content-center flex-column">
+                <h5 class="card-title"><?= $element[0]; ?></h5>
+                <p class="card-text"><?= $element[2]; ?></p>
+                <a href="list.php" class="btn btn-primary">List de bonnets</a>
+              </div>
+            </div>
+
+            <?php
+            if ($count >= 3) {
+                break;
             }
-        ?>
-    </table>
+        }
+    ?>
+</div>
+
 <?php
-require_once './includes/footer.php'
+    include_once './includes/footer.php';
 ?>
-
-
-<!-- 9. Header
-
-    L'idée est ici de regrouper la plus grande partie du code commun, pour l'inclure dans toutes nos pages (et donc, réduire le nombre de fois où on a besoin de l'écrire).
-    Dans header.php, déplacer les includes des fichiers de variables et de fonctions (remplacer les includes par des require_once et rechercher ce que fait cette fonction)
-    Ajouter le html qui va être commun à toutes nos pages (balises <html>, <head>, <body>, etc.)
-    Inclure le fichier header.php dans la page de liste des bonnets
-
-    10. Footer
-    Dans footer.php, ajouter le html commun à toutes nos pages se trouvant après le php
-    Inclure ce fichier footer.php dans la page de liste
-    -->
