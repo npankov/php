8b151961fab0b61a8c2039d0773cf9ef825f0fe2
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-28 11:06:32 +0100

    exercise 16 refactoring

--- includes/header.php
+++ includes/header.php
@@ -14,14 +14,16 @@ if (isset($_POST['email'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+          integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="index.php">Lucky Bonnet</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -36,11 +38,11 @@ if (isset($_POST['email'])) {
                     <?php
                     if (isset($_SESSION['email'])) {
                         ?>
-                            <a class="nav-link disabled" href="#"><?= $_SESSION['email']; ?></a>
+                        <a class="nav-link disabled" href="#"><?= $_SESSION['email']; ?></a>
                         <?php
                     } else {
                         ?>
-                            <a class="nav-link" aria-current="page" href="login.php">Connexion</a>
+                        <a class="nav-link" aria-current="page" href="login.php">Connexion</a>
                         <?php
                     }
                     ?>
--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-    include_once './includes/header.php';
+include_once './includes/header.php';
 ?>
 
-<div class="row">
-    <?php
+    <div class="row">
+        <?php
         $count = 0;
         foreach ($myArray as $element) {
             $count++;
             ?>
 
             <div class="card mx-2 com" style="width: 18rem;">
-              <img src="<?= $element[3]; ?>" class="card-img-top" alt="...">
-              <div class="card-body d-flex justify-content-center flex-column">
-                <h5 class="card-title" style="font-size: 1.2rem"><?= $element[0]; ?></h5>
-                <p class="card-text"><?= $element[2]; ?></p>
-                <a href="list.php" class="btn btn-primary">List de bonnets</a>
-              </div>
+                <img src="<?= $element[3]; ?>" class="card-img-top" alt="...">
+                <div class="card-body d-flex justify-content-center flex-column">
+                    <h5 class="card-title" style="font-size: 1.2rem"><?= $element[0]; ?></h5>
+                    <p class="card-text"><?= $element[2]; ?></p>
+                    <a href="list.php" class="btn btn-primary">List de bonnets</a>
+                </div>
             </div>
 
             <?php
@@ -23,9 +23,9 @@
                 break;
             }
         }
-    ?>
-</div>
+        ?>
+    </div>
 
 <?php
-    include_once './includes/footer.php';
+include_once './includes/footer.php';
 ?>
